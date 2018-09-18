530a55f6cded5c4a3f0e367422f7f4f263ad7ba8
przemyslaw.w.michalak <przemyslaw.w.michalak@student.put.poznan.pl>
2018-09-18 21:53:29 +0200

    better jlink script

--- ecliipse_external_tool_programmer/stm32_program_jlink.bat
+++ ecliipse_external_tool_programmer/stm32_program_jlink.bat
@@ -2,4 +2,21 @@
 @echo przenosze plik, zeby mial rozszerzenie bin, bo JLinkowi nie podoba sie *.binary
 cp %1 %2
 
+@IF "%errorlevel%" NEQ "0" goto copyerror
+
 "C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.0.0\Servers\J-Link_gdbserver\JLink.exe" -commanderscript %3
+@IF "%errorlevel%" NEQ "0" goto jlinkerror
+
+@goto success
+
+:copyerror
+@echo Wystapil problem z kopiowaniem plikow!!!
+@Exit
+
+:jlinkerror
+@echo Wystapil problem z programowaniem!
+@Exit
+
+:success
+@echo Programowanie powiodlo sie!
+@Exit
